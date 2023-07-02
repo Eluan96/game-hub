b85306ea212087f45b03ced4ff9e760845754525
Eluan96 <eluan.ea@gmail.com>
2023-07-03 00:41:49 +0100

    Refactor remove duplicated styles

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width="400px" borderRadius={10} overflow="hidden" height="350px">
+    <Card height="350px">
       <Skeleton height="200px">
         <CardBody>
           <SkeletonText />
